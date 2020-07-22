81349cc1e8b4addd2a552327bc0be9978fe08d95
Workgeek100 <maybha_70@hotmail.com>
2020-07-22 18:24:56 +0300

    P45

--- js/obstacles.js
+++ js/obstacles.js
@@ -24,7 +24,7 @@ class Obstacles{
         image(this.image3,displayWidth-500,displayHeight/2+30,160,160);
         image(this.image4,displayWidth-700,displayHeight/2+30,160,160);
         image(this.image5,displayWidth-900,displayHeight/2+30,230,160);
-        image(this.image6,displayWidth-600,displayHeight/2+30,300,160);
+        image(this.image6,displayWidth-800,displayHeight/2+10,300,350);
         image(this.image7,displayWidth-400,displayHeight/2+50,300,130);
         image(this.image8,displayWidth-200,displayHeight/2+100,100,50);
     }
--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ var player, playerRunning, playerRunning_img;
 var office, ground, intro, obstacles;
 
 function preload(){
-office = loadImage("images/bg1.jpg");
+office = loadImage(("images/bg5.jpg"));
 playerRunning_img = loadAnimation("images/running1.png","images/running2.png","images/running3.png","images/running4.png","images/running5.png");
 }
 function setup() {
@@ -18,7 +18,7 @@ function setup() {
 
   player = new Player(displayWidth/10,displayHeight/2+70,200,200);
   ground = new Ground(displayWidth/2,displayHeight/2+150,10000,50);
-  //intro = new Intro();
+  intro = new Intro();
   obstacles = new Obstacles(displayWidth-200,displayHeight/2-50);
   
 }
